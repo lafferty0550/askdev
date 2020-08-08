24c1ab5ad96af29076af2e1e9b68534db8f8e9e6
Ilya Mashinin <lafferty0550@gmail.com>
2020-08-09 01:29:56 +0300

    make it better

--- src/server/launcher.ts
+++ src/server/launcher.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production')
+    require('dotenv').config();
 
 const app = express();
 
